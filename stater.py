6383aa146f39f47f291e941d1bac6e2a3db8f198
josepicoli <josepicoli42@gmail.com>
2024-01-03 19:36:43 -0300

    refactoring all functions into a single one

--- stater.py
+++ stater.py
@@ -6,70 +6,29 @@ if len(sys.argv) > 2:
     print("many arguments")
     sys.exit()
 
-def get_usd(): #Dólar Americano
-    try:
-        r = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL")
-        usd = r.json()['USDBRL']['bid']
-        print(f"USD = {usd}")
-    except:
-        print("error")
+def get_value(coin: str):
+    coin = coin.upper()
+    url = f"https://economia.awesomeapi.com.br/last/{coin}-BRL"
+    r = requests.get(url)
+    value = r.json()[f'{coin}BRL']['bid']
+    print(f"{coin} = {value}")
 
-def get_eur(): #Euro
-    try:
-        r = requests.get("https://economia.awesomeapi.com.br/last/EUR-BRL")
-        eur = r.json()['EURBRL']['bid']
-        print(f"EUR = {eur}")
-    except:
-        print("error")
-
-def get_btc(): #Bitcoin
-    try:
-        r = requests.get("https://economia.awesomeapi.com.br/last/BTC-BRL")
-        btc = r.json()['BTCBRL']['bid']
-        print(f"BTC = {btc}")
-    except:
-        print("error")
-
-def get_ars(): #Peso Argentino
-    try:
-        r = requests.get("https://economia.awesomeapi.com.br/last/ARS-BRL")
-        ars = r.json()['ARSBRL']['bid'] #peso argentino
-        print(f"ARS = {ars}")
-    except:
-        print("error")
-
-def get_jpy(): #Iene Japonês
-    try:
-        r = requests.get("https://economia.awesomeapi.com.br/last/JPY-BRL")
-        jpy = r.json()['JPYBRL']['bid']
-        print(f"JPY = {jpy}")
-    except:
-        print("error")
-
-def get_eth(): #Ethereum
-    try:
-        r = requests.get("https://economia.awesomeapi.com.br/last/ETH-BRL")
-        eth = r.json()['ETHBRL']['bid']
-        print(f"ETH = {eth}")
-    except:
-        print("error")
-
-def get_cny(): #Yuan Chinês
-    try:
-        r = requests.get("https://economia.awesomeapi.com.br/last/CNY-BRL")
-        cny = r.json()['CNYBRL']['bid']
-        print(f"CNY = {cny}")
-    except:
-        print("error")
+# usd -> Dólar Americano
+# eur -> Euro
+# btc -> Bitcoin
+# ars -> Peso Argentino
+# jpy -> Iene Japonês
+# eth -> Ethereum
+# cny -> Yuan Chinês
 
 def get_all():
-    get_usd()
-    get_eur()
-    get_btc()
-    get_ars()
-    get_jpy()
-    get_eth()
-    get_cny()
+    get_value("usd")
+    get_value("eur")
+    get_value("btc")
+    get_value("ars")
+    get_value("jpy")
+    get_value("eth")
+    get_value("cny")
 
 def main():
     sys.argv.pop(0)
@@ -80,19 +39,19 @@ def main():
         case ['--all']:
             get_all()
         case ['--usd']:
-            get_usd()
+            get_value("usd")
         case ['--eur']:
-            get_eur()
+            get_value("eur")
         case ['--btc']:
-            get_btc()
+            get_value("btc")
         case ['--ars']:
-            get_ars()
+            get_value("ars")
         case ['--jpy']:
-            get_jpy()
+            get_value("jpy")
         case ['--eth']:
-            get_eth()
+            get_value("eth")
         case ['--cny']:
-            get_cny()
+            get_value("cny")
         case _:
             print("invalid command")
 
